a2aab37424d4ef2e7c0436a26cd3764dddd24f5e
steefanyalvees <stefanyanjos@hotmail.com>
2021-05-10 13:35:03 +0100

    Update Paid.java

--- src/Paid.java
+++ src/Paid.java
@@ -68,6 +68,11 @@ public class Paid extends javax.swing.JFrame {
         });
 
         jButton2.setText("NO");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -167,6 +172,10 @@ public class Paid extends javax.swing.JFrame {
         
       
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
