1e50bfa67e2aa086faec2f745aa35ba4390c3161
coesama <coemaciel@gmail.com>
2021-05-10 18:35:54 +0100

    Connection Class, Operations and Credit validation
    A public connection class created, Operation class created to validate a credit card number stored in a DB.

---
+++ src/Operations.java
@@ -0,0 +1,43 @@
+
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author coema
+ */
+public class Operations {
+
+    private static Component frame;
+    public static boolean isCredit(String cname, String cnumber, String seccode){
+        try{
+            Connection myConn = MySqlConnection.getConnection();
+            String mySqlQuery = "SELECT name, cnumber, secode FROM card WHERE name = '"+cname+"' AND cnumber = '"+cnumber+"' AND secode = '"+seccode+"'";
+            PreparedStatement preparedStatement = myConn.prepareStatement(mySqlQuery);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while(resultSet.next()){
+                CreditSession.cname = resultSet.getString("name");
+                CreditSession.cnumber = resultSet.getString("cnumber");
+                CreditSession.seccode = resultSet.getString("secode");
+                
+                return true;
+            }
+        }catch (Exception exception){
+            JOptionPane.showMessageDialog(frame, "Database error: " + exception.getMessage());
+           }
+        
+        return false;
+    }
+
+       
+}
